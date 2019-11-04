347710cd683966d0a9182e9b4adcdf35991f402f
gaoxinxiao <gaoxinxiao@bicai365.com>
2019-11-04 17:59:33 +0800

    登录完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,112 @@
-import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+// import './list.dart'; //list view组件 与官网一样
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: '蛤蟆金服',
-      theme: new ThemeData(
-        primaryColor: Colors.green
+    return MaterialApp(
+      title: '登录',
+      theme: ThemeData(primaryColor: Colors.white),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: BackButton(color: Colors.grey),
+          title: Text('登录'),
+        ),
+        body: Input(),
       ),
-      home: new RandomWords(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class Input extends StatefulWidget {
   @override
-  createState() => new RandomWordsState();
+  _InputState createState() => _InputState();
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-
-  final _saved = new Set<WordPair>();
-
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return new Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
-      final tiles = _saved.map((pair) {
-        return new ListTile(
-          title: new Text(pair.asPascalCase, style: _biggerFont),
-        );
-      });
-      final divided =
-          ListTile.divideTiles(context: context, tiles: tiles).toList();
-      return new Scaffold(
-          appBar: new AppBar(
-            title: new Text('收藏夹'),
-          ),
-          body: new ListView(
-            children: divided,
-          ));
-    }));
-  }
+class _InputState extends State<Input> {
+  final TextEditingController _phone = new TextEditingController();
+  final TextEditingController _yzm = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('蛤蟆金服'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+  
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 20.0, 0, 0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Row(children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(8.0, 4.0, 0, 0),
+                child: Text(
+                  '手机号：',
+                  style: new TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ),
+              Container(
+                  width: 300,
+                  child: Theme(
+                    //设置主题 边框线等等。。。
+                    data: new ThemeData(primaryColor: Colors.green),
+                    child: TextField(
+                      controller: _phone,
+                      keyboardType: TextInputType.phone, //键盘类型
+                      cursorColor: Colors.grey, //光标颜色
+                      decoration: InputDecoration(hintText: '请输入手机号'),
+                    ),
+                  )),
+            ]),
+          ),
+          Container(
+            child: Row(children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(8.0, 4.0, 0, 0),
+                child: Text(
+                  '验证码：',
+                  style: new TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ),
+              Container(
+                  width: 300,
+                  child: Theme(
+                    data: new ThemeData(primaryColor: Colors.green),
+                    child: TextField(
+                      controller: _yzm,
+                      cursorColor: Colors.grey,
+                      keyboardType: TextInputType.number, //键盘类型
+                      decoration: InputDecoration(
+                        hintText: '请输入验证码',
+                      ),
+                    ),
+                  )),
+            ]),
+          ),
+          Container(
+            width: 200,
+            margin: const EdgeInsets.fromLTRB(0.0, 20.0, 0, 0),
+            child: RaisedButton(
+              onPressed: () {
+                var yzm = _yzm.text;
+                var phone = _phone.text;
+                print('$yzm');
+                print('$phone');
+             
+              },
+              child: Text('登录'),
+              textColor: Colors.white,
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4.0)),
+            ),
+          )
         ],
       ),
-      body: _buildSuggestions(),
     );
   }
 }
